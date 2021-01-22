5e4204e40db1343d5a49634122574138af05b3ed
13281276736@189.com <1044562118@qq.com>
2021-01-23 00:57:23 +0800

    Initial commit

---
+++ cats.py
@@ -0,0 +1,22 @@
+from animal import Animal
+
+
+class Cats(Animal):
+
+    def __init__(self):
+        self.wool = "短毛哦"
+        super().__init__("Tom","蓝色","2岁","雄性")
+
+    def catch(self):
+        print(f"{self.name}抓老鼠很厉害")
+
+    def shout(self):
+        shout = "喵喵喵"
+        print(f"{self.name}叫起来是{shout}")
+
+if __name__ == '__main__':
+    cat = Cats()
+    cat.run()
+    cat.catch()
+    cat.shout()
+
